5bb2bf56b3b0c1743f33ba0218a2f57551220ea5
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-03-05 16:53:50 +0300

    Первоначальная настройка MVC и многоуровневая арихитектура.

---
+++ VetClinicSolution/VetClinic.DAL/VetClinicDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VetClinic.DAL
+{
+    public class VetClinicDbContext : DbContext
+    {
+        public VetClinicDbContext(DbContextOptions<VetClinicDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ VetClinicSolution/VetClinic/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using VetClinic.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("Pgsql");
+
+builder.Services.AddDbContext<VetClinicDbContext>(options => 
+{
+    options.UseNpgsql(connectionString);
+});
+
+var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+app.UseStatusCodePages();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name:"default", 
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.Run();
